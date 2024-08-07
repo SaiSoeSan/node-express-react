16731803b44a1da083ab5b5d28f58ad10f738202
SaiSoeSan <33777412+SaiSoeSan@users.noreply.github.com>
2024-08-07 06:47:06 -0500

    quiz 3

--- express-react/server/app.js
+++ express-react/server/app.js
@@ -4,9 +4,12 @@ const PORT = 8000;
 let path = require('path')
 const cors = require('cors');
 
+let selectedLanguage = '';
+let selectedData = '';
+
 // Define CORS options
 const corsOptions = {
-    origin: 'http://localhost:3006', // Replace with the allowed origin
+    origin: 'http://localhost:3000', // Replace with the allowed origin
     methods: 'GET,POST,DELETE,PUT',
     allowedHeaders: 'Content-Type',
 };
@@ -23,6 +26,41 @@ app.use(cors(corsOptions));
 //Read the parameters from post request
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+
+//quiz3
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,"../client/build/language.html"))
+})
+
+app.post('/skills',(req,res) => {
+    let language = req.body.language;
+    selectedLanguage = language;
+    if(language == 'Java'){
+        res.sendFile(path.join(__dirname,"../client/build/skillJava.html"))
+    }else{
+        res.sendFile(path.join(__dirname,"../client/build/skillJavascript.html"))
+    }
+});
+
+app.post('/preview',(req,res) => {
+    if(selectedLanguage == 'Java'){
+        selectedData = {
+            "Core Java" : req.body.skill,
+            "Java Build Tools" : req.body.buildTool
+        }
+    }else{
+        selectedData = {
+            "HTML / CSS" : req.body.skill,
+            "Source Control" : req.body.buildTool
+        }
+    }
+    console.log(selectedData)
+    res.sendFile(path.join(__dirname,"../client/build/index.html"))
+})
+
+app.get('/api/skill',(req,res) => {
+    res.json({data:selectedData})
+})
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
@@ -64,11 +102,14 @@ app.put('/api/item/:id', (req, res) => {
     res.json({data: items})
 });
 
-app.get('*', (req, res) => {
+// app.get('*', (req, res) => {
 
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 
-});
+// });
+
+
+
 
 
 app.listen(PORT,() => {
